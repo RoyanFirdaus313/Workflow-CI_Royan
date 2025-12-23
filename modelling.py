0a695278af071b715d710b9581527b5e05a3542c
Royan Firdaus <91227794+RoyanFirdaus313@users.noreply.github.com>
2025-12-24 00:30:20 +0700

    Update modelling.py

--- modelling.py
+++ modelling.py
@@ -1,27 +1,35 @@
 import argparse
 import pandas as pd
-from sklearn.linear_model import ElasticNet
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import accuracy_score
 import mlflow
 import mlflow.sklearn
 
+# Parsing argumen
 parser = argparse.ArgumentParser()
-parser.add_argument("--alpha", type=float, default=0.5)
-parser.add_argument("--l1_ratio", type=float, default=0.5)
+parser.add_argument("--C", type=float, default=1.0)
 args = parser.parse_args()
 
+# Load dataset
 data = pd.read_csv("MLProject/namadataset_preprocessing/data.csv")
-X = data.drop("target", axis=1)
-y = data["target"]
 
+# Pisahkan fitur dan target
+X = data.drop("Berlangganan Deposito", axis=1)
+y = data["Berlangganan Deposito"]
+
+# Split data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model = ElasticNet(alpha=args.alpha, l1_ratio=args.l1_ratio)
+# Training model
+model = LogisticRegression(C=args.C, max_iter=1000)
 model.fit(X_train, y_train)
-preds = model.predict(X_test)
-rmse = mean_squared_error(y_test, preds, squared=False)
 
+# Prediksi dan evaluasi
+preds = model.predict(X_test)
+acc = accuracy_score(y_test, preds)
+
+# Logging ke MLflow
 mlflow.sklearn.log_model(model, "model")
-mlflow.log_metric("rmse", rmse)
-print(f"Model trained. RMSE: {rmse}")
+mlflow.log_metric("accuracy", acc)
+print(f"Model trained. Accuracy: {acc}")
